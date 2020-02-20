95fd733ce760d9a142935576c9e85ddc646f51b3
shutong <123@qq.com>
2020-02-20 17:01:04 +0800

    book

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+      <router-view />
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  padding: 0;
+  margin: 0;
+  list-style: none;
 }
 
-#nav {
-  padding: 30px;
+ul,
+ol {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+a {
+  margin: 0;
+  padding: 0;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.fr {
+  float: right;
+}
+
+.fl {
+  float: left;
+}
+
+.clearfix::before,
+.clearfix::after {
+  clear: both;
+  content: "";
+  display: block;
+}
+.auto-img {
+  width: 100%;
+  display: block;
 }
 </style>
